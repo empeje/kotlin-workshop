8f31c0a7dd771dbb3599e15d99c05e362eceec1d
marcinpanek <marcin.panek@glovoapp.com>
2022-06-12 19:16:00 +0200

    setup

--- src/test/kotlin/refactored/api/EndpointTest.kt
+++ src/test/kotlin/refactored/api/EndpointTest.kt
@@ -6,71 +6,70 @@ import assertk.assertions.isNull
 import common.DomainObject
 import common.DomainObjectApiResponse
 import org.junit.jupiter.api.Test
-import refactored.domain.DomainServiceImpl
 import refactored.testdoubles.MockDomainRepository
 
 class EndpointTest {
 
-    private val repository = MockDomainRepository()
-    private val endpoint = Endpoint(DomainServiceImpl(repository))
-
-    @Test
-    fun `should return response entity with domain object`() {
-        // given
-        repository returns DomainObject(1L, "name")
-
-        // when
-        val response = endpoint.handle()
-
-        // then
-        assertThat(response).apply {
-            transform { it.statusCode }.isEqualTo(200)
-            transform { it.body }.isEqualTo(DomainObjectApiResponse(1L, "name"))
-        }
-    }
-
-    @Test
-    fun `should return response entity with 404 status code`() {
-        // given
-        repository.throwsNotFoundDomainException()
-
-        // when
-        val response = endpoint.handle()
-
-        // then
-        assertThat(response).apply {
-            transform { it.statusCode }.isEqualTo(404)
-            transform { it.body }.isNull()
-        }
-    }
-
-    @Test
-    fun `should return response entity with 400 status code`() {
-        // given
-        repository.throwsValidationDomainException()
-
-        // when
-        val response = endpoint.handle()
-
-        // then
-        assertThat(response).apply {
-            transform { it.statusCode }.isEqualTo(400)
-            transform { it.body }.isNull()
-        }
-    }
-
-    @Test
-    fun `should return response entity with 500 status code`() {
-        // given
-        repository.throwsRuntimeException()
-
-        // when
-        val response = endpoint.handle()
-
-        // then
-        assertThat(response).apply {
-            transform { it.statusCode }.isEqualTo(500)
-            transform { it.body }.isNull()
-        }
-    }
+//    private val repository = MockDomainRepository()
+//    private val endpoint = TODO()
+//
+//    @Test
+//    fun `should return response entity with domain object`() {
+//        // given
+//        repository returns DomainObject(1L, "name")
+//
+//        // when
+//        val response = endpoint.handle()
+//
+//        // then
+//        assertThat(response).apply {
+//            transform { it.statusCode }.isEqualTo(200)
+//            transform { it.body }.isEqualTo(DomainObjectApiResponse(1L, "name"))
+//        }
+//    }
+//
+//    @Test
+//    fun `should return response entity with 404 status code`() {
+//        // given
+//        repository throws NotFoundDomainException()
+//
+//        // when
+//        val response = endpoint.handle()
+//
+//        // then
+//        assertThat(response).apply {
+//            transform { it.statusCode }.isEqualTo(404)
+//            transform { it.body }.isNull()
+//        }
+//    }
+//
+//    @Test
+//    fun `should return response entity with 400 status code`() {
+//        // given
+//        repository throws ValidationDomainException()
+//
+//        // when
+//        val response = endpoint.handle()
+//
+//        // then
+//        assertThat(response).apply {
+//            transform { it.statusCode }.isEqualTo(400)
+//            transform { it.body }.isNull()
+//        }
+//    }
+//
+//    @Test
+//    fun `should return response entity with 500 status code`() {
+//        // given
+//        repository throws RuntimeException()
+//
+//        // when
+//        val response = endpoint.handle()
+//
+//        // then
+//        assertThat(response).apply {
+//            transform { it.statusCode }.isEqualTo(500)
+//            transform { it.body }.isNull()
+//        }
+//    }
 }
--- src/test/kotlin/refactored/testdoubles/MockDomainRepository.kt
+++ src/test/kotlin/refactored/testdoubles/MockDomainRepository.kt
@@ -4,20 +4,11 @@ import common.DomainObject
 import common.DomainRepository
 import io.mockk.every
 import io.mockk.mockk
-import refactored.domain.DomainException
 
 class MockDomainRepository(private val mock: DomainRepository = mockk()) : DomainRepository by mock {
 
-    fun throwsRuntimeException() {
-        every { mock.findDomainObject() } throws RuntimeException()
-    }
-
-    fun throwsNotFoundDomainException() {
-        every { mock.findDomainObject() } throws DomainException.NotFound("info")
-    }
-
-    fun throwsValidationDomainException() {
-        every { mock.findDomainObject() } throws DomainException.Validation("info")
+    infix fun throws (e: Exception) {
+        every { mock.findDomainObject() } throws e
     }
 
     infix fun returns(domainObject: DomainObject) {
