2c87a9f4088c7d0a9467a4eedf7b6f7d62ff9cff
marcinpanek <marcin.panek@glovoapp.com>
2022-06-12 10:59:58 +0200

    setup

--- src/main/kotlin/refactored/DiscountCalculator.kt
+++ src/main/kotlin/refactored/DiscountCalculator.kt
@@ -1,16 +1,19 @@
 package refactored
 
+import java.lang.RuntimeException
+
 class DiscountCalculator(
     val logger: Logger = Logger.Sout
 ) {
 
     fun calculateDiscount(money: Money, strategy: DiscountStrategy): Money {
-        when(strategy) {
-            is DiscountStrategy.Fixed -> {
-                val (amount, currency) = (strategy.value)
-                logger.logInfo("Calculating fixed discount: $amount ${currency.value}")
-            }
-            is DiscountStrategy.Percentage -> logger.logInfo("Calculating percentage discount: ${strategy.percent.value}")
+        if (strategy is DiscountStrategy.Fixed) {
+            val (amount, currency) = (strategy.value)
+            logger.logInfo("Calculating fixed discount: $amount ${currency.value}")
+        } else if (strategy is DiscountStrategy.Percentage) {
+            logger.logInfo("Calculating percentage discount: ${strategy.percent.value}")
+        } else {
+            throw RuntimeException("Unknown strategy type")
         }
         return strategy.calculateDiscountedValue(money)
     }
